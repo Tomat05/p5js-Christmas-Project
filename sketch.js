4609f1a5c122c59643411a5d862ad01eaed64972
Tomat05 <wetherill.t@outlook.com>
2023-01-03 13:11:35 +0000

    Added intro video

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
-let scene = 1;
+let scene = 0;
 
 let grid;
 let screenMask;
+let intro;
+
+function preload() {
+    intro = createVideo('src/resources/intro.mov');
+}
 
 // Initial setup
 function setup() {
@@ -9,11 +14,27 @@ function setup() {
     screenMask = new ScreenMask();
     grid = new Grid();
     grid.createGrid();
+    intro.hide();
+
+    try {
+        intro.play();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function onIntroFinish() {
+    scene = 1;
+}
+
+function introCutscene() {
+    background(0);
+    image(intro, 0, 0);
+    intro.onended(onIntroFinish);
 }
 
 function mainGame() {
     background(0);
-    frameRate(0);
     noStroke();
 
     grid.update();
@@ -24,7 +45,33 @@ function mainGame() {
     rect((windowWidth / 2) - 25, (windowHeight / 2) - 25, 50, 50);
     pop();
     // screenMask.draw();
+}
 
+// Update loop (FRAMERATE DEPENDANT)
+function draw() {
+    switch (scene) {
+        case 0:
+            introCutscene();
+            break;
+        case 1:
+            // TODO: Menu screen
+            background(0);
+            push();
+            fill(255);
+            textSize(100);
+            text("Menu", 100, 100, 200, 200);
+            pop();
+            break;
+        case 2:
+            mainGame();
+            break;
+        case 3:
+            // TODO: End cutscene
+            break;
+        default:
+            scene = 1;
+            break;
+    }
 
     push();
     let fr = Math.round(frameRate() * 10) / 10;
@@ -32,24 +79,6 @@ function mainGame() {
     textSize(20);
     text(fr, 10, 10, 20, 20);
     pop();
-}
-
-// Update loop (FRAMERATE DEPENDANT)
-function draw() {
-    switch (scene) {
-        case 0:
-            // TODO: Intro cutscene
-            break;
-        case 1:
-            mainGame();
-            break;
-        case 2:
-            // TODO: End cutscene
-            break;
-        default:
-            scene = 1;
-            break;
-    }
 }
 
 // I hate that I have to put this outside of my player class
